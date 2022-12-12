6a08d61f85f6c8f41308347ed8b5195928dc70f3
Kevin Dong <dong.kevin1@gmail.com>
2022-12-11 23:50:40 -0600

    Add day12

---
+++ day12/day12p1.py
@@ -0,0 +1,62 @@
+import collections
+
+
+def parse_board():
+    with open("sample.txt", "r") as f:
+        data = f.readlines()
+    output = []
+    start = None
+    end = None
+    for row, line in enumerate(data):
+        output.append([])
+        for column, c in enumerate(line):
+            if c == "\n":
+                continue
+            elif c == "S":
+                output[-1].append(ord("a"))
+                start = (row, column)
+            elif c == "E":
+                output[-1].append(ord("z"))
+                end = (row, column)
+            else:
+                output[-1].append(ord(c))
+    return (output, start, end)
+
+
+def append_adjacent(board, steps, x, y, seen):
+    output = []
+    movements = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+    for (dx, dy) in movements:
+        new_x, new_y = x + dx, y + dy
+        if (new_x, new_y) in seen:
+            continue
+        elif not (0 <= new_x < len(board)) or not (0 <= new_y < len(board[0])):
+            continue
+        old_height = board[x][y]
+        new_height = board[new_x][new_y]
+        if steps[new_x][new_y] is None and new_height - old_height <= 1:
+            output.append((new_x, new_y, steps[x][y] + 1))
+            seen.add((new_x, new_y))
+    return output
+
+
+def compute_fewest_steps(board, start, end):
+    steps = [[None for _ in range(len(board[0]))] for _ in range(len(board))]
+    steps[start[0]][start[1]] = 0
+    queue = collections.deque()
+    seen = set()
+    queue.append((start[0], start[1]))
+    while len(queue) > 0:
+        (x, y) = queue.popleft()
+        new_nodes = append_adjacent(board, steps, x, y, seen)
+        for (new_x, new_y, new_steps) in new_nodes:
+            if (new_x, new_y) == end:
+                print(f"Finished: {new_steps}")
+                break
+            steps[new_x][new_y] = new_steps
+            queue.append((new_x, new_y))
+    print(steps[end[0]][end[1]])
+
+
+board, start, end = parse_board()
+print(compute_fewest_steps(board, start, end))
